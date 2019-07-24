cf5133caf7558dacb1f22f7ff0e004267a425be4
Carlos Villalobos <charlievilla@gmail.com>
2019-07-24 14:31:45 -0600

    Clase 13 nueva constante y toFixed(1)

--- clase13.js
+++ clase13.js
@@ -15,19 +15,20 @@ function aumentartDePeso (persona) {
 
 esto equivalente a la que sigue
 **/
-const INCREMENTO_PESO = 200
+const INCREMENTO_PESO = 0.2
+const DIAS_DEL_ANO = 365
 
 const aumentarDePeso = persona => persona.peso += INCREMENTO_PESO
 const adelgazar = persona => persona.peso -= INCREMENTO_PESO
 
-for  (var i = 1; i <= 365; i++) {
+for  (var i = 1; i <= DIAS_DEL_ANO; i++) {
     var random = Math.random()
 
     if (random < 0.25) {
         aumentarDePeso(sacha)
     } else if (random < 0.5) {
-        adelgazar
+        adelgazar(sacha)
     }
 }
 
-console.log(`Al final de año ${sacha.nombre} pesa ${sacha.peso}kg`)
+console.log(`Al final de año ${sacha.nombre} pesa ${sacha.peso.toFixed(1)}kg`)
