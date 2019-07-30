935fc50b8d744277060adee7bcea23dada7b8909
charlievillacr <charlievilla@gmail.com>
2019-07-30 11:49:47 -0600

    Corregir redacción del console.log

--- clase15.js
+++ clase15.js
@@ -1,15 +1,22 @@
 //Contador de veces que llueve
 var contador = 0
 
-
 /**
-Definir una función que nos va devolver si está lloviendo o no
+Definir una función
+que nos va devolver
+si está lloviendo o no
 **/
 const llueve = () =>  Math.random() < 0.25
 
 // Ciclo do-while
 do {
     contador++
-} while (!llueve())
+} while (!llueve()) {
+    if(contador > 1) {
+        console.log(`Fui a ver si llovía ${contador} veces`)
+    } else {
+        console.log(`Fui a ver si llovía ${contador} ves`)
+    }
+}
 
-console.log(`Fui a ver si llovía ${contador} veces`)
+
