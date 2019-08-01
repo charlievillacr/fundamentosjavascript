c24a40c056d26e819aba22883fab23e0060f2498
charlievillacr <charlievilla@gmail.com>
2019-07-31 22:38:12 -0600

    Chuck aprende a filtrar

---
+++ clase18.js
@@ -0,0 +1,52 @@
+var sacha = {
+    nombre: 'Sacha',
+    apellido: 'Lifszyc',
+    altura: 1.72
+}
+var allan = {
+    nombre: 'Alan',
+    apellido: 'Perez',
+    altura: 1.86
+}
+var martin = {
+    nombre: 'Martin',
+    apellido: 'Gomez',
+    altura: 1.85
+}
+var dario = {
+    nombre: 'Dario',
+    apellido: 'Juarez',
+    altura: 1.71
+}
+var vicky = {
+    nombre: 'Vicky',
+    apellido: 'Zapata',
+    altura: 1.56
+}
+var paula = {
+    nombre: 'Paula',
+    apellido: 'Barros',
+    altura: 1.76
+}
+
+var personas = [sacha, allan, martin, dario, vicky, paula]
+
+// para filtrar se necesitan dos cosas
+// una condición y un array
+
+
+
+
+/**
+for (var i = 0; i < personas.length; i++) {
+    var persona = personas[i]
+    console.log(`${persona.nombre} mide ${persona.altura}mts`)
+}
+
+// Formas de "acceder" la info de las variables / personas
+
+
+personas[0] // 0 "Cero" va ser el primero de los elementos = Sacha
+personas[0].altura // 1.72
+personas[0]["altura"] // 1.72
+**/
